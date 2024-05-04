46aa300b5d398d2911a9744f0c89fea6e40dbe55
Gabriel Tenório de Oliveira <gabrieltenorio17@gmail.com>
2024-05-04 13:13:35 -0300

    Finalização do projeto

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,25 @@
 // Captura dos itens
-const menuBurguer = document.querySelector('#btn-burguer');
-const nav = document.querySelector('#nav');
-const links = document.querySelectorAll('.menu-link');
+const menuBurguer = document.querySelector("#btn-burguer");
+const nav = document.querySelector("#nav");
+const links = document.querySelectorAll(".menu-link");
+const tela = screen.width;
 
 // Funcionalidade do botão menu hamburguer
-menuBurguer.addEventListener('click', () =>{
-    nav.classList.toggle('active');
+menuBurguer.addEventListener("click", () => {
+  nav.classList.toggle("active");
 });
 
 // Funcionalidade da menu - item selecionado
-function selectedLink(){
-    links.forEach((item)=>{
-        item.classList.remove('selected')
-    })
-    this.classList.add('selected')
-    nav.classList.toggle('active')
+function selectedLink() {
+  if (tela < 1024) { // Correção aqui
+    links.forEach((item) => {
+      item.classList.remove("selected");
+    });
+    this.classList.add("selected");
+    nav.classList.toggle("active");
+  }
 }
 
-links.forEach((item)=>{
-    item.addEventListener('click', selectedLink)
-})
+links.forEach((item) => {
+  item.addEventListener("click", selectedLink);
+});
